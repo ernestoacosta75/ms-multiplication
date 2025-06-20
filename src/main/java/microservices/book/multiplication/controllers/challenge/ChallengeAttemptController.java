25ab36b8e67d4079a9eb646e54c20f892070e57c
ernestoacosta75 <ernestoacostacuba@yahoo.com.mx>
2025-06-20 14:44:42 +0200

    Implementing ChallengeAttemptController test and final controller logic

--- src/main/java/microservices/book/multiplication/controllers/challenge/ChallengeAttemptController.java
+++ src/main/java/microservices/book/multiplication/controllers/challenge/ChallengeAttemptController.java
@@ -2,7 +2,12 @@ package microservices.book.multiplication.controllers.challenge;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import microservices.book.multiplication.domain.ChallengeAttempt;
+import microservices.book.multiplication.dtos.challenge.ChallengeAttemptDto;
 import microservices.book.multiplication.service.interfaces.challenge.IChallengeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/attempts")
 public class ChallengeAttemptController {
     private final IChallengeService challengeService;
+    
+    @PostMapping
+    ResponseEntity<ChallengeAttempt> postResult(@RequestBody ChallengeAttemptDto challengeAttemptDto) {
+        return ResponseEntity.ok(challengeService.verifyAttempt(challengeAttemptDto));
+    }
 }
