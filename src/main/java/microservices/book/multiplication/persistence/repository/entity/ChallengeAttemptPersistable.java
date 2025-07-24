c0fe786a0e2f1d953d668882341372790aaafc5d
Ernesto Antonio Rodriguez Acosta (MSC Technology Italia) <ernestoantonio.rodriguezacosta@msc.com>
2025-07-24 14:06:02 +0200

    Adding jpa dependencies. Adding JPA attributes on entities

--- src/main/java/microservices/book/multiplication/persistence/repository/entity/ChallengeAttemptPersistable.java
+++ src/main/java/microservices/book/multiplication/persistence/repository/entity/ChallengeAttemptPersistable.java
@@ -1,15 +1,20 @@
 package microservices.book.multiplication.persistence.repository.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+
 @ToString
 @Setter
 @Getter
 @Entity
-//@Table(name = "challenge_attempt")
+@Table(name = "challenge_attempt")
 public class ChallengeAttemptPersistable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
--- src/main/java/microservices/book/multiplication/persistence/repository/jpa/IChallengeAttemptJpaRepository.java
+++ src/main/java/microservices/book/multiplication/persistence/repository/jpa/IChallengeAttemptJpaRepository.java
@@ -2,6 +2,7 @@ package microservices.book.multiplication.persistence.repository.jpa;
 
 import microservices.book.multiplication.persistence.repository.entity.ChallengeAttemptPersistable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface IChallengeAttemptJpaRepository extends JpaRepository<ChallengeAttemptPersistable, Long> {
 }
