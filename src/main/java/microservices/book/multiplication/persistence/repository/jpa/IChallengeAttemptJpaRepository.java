aca29cf1481aa6ebbdb5544c41aef4611898ebbc
Ernesto Antonio Rodriguez Acosta (MSC Technology Italia) <ernestoantonio.rodriguezacosta@msc.com>
2025-07-30 16:42:17 +0200

    Fixing database jdbc url

--- src/main/java/microservices/book/multiplication/persistence/repository/jpa/IChallengeAttemptJpaRepository.java
+++ src/main/java/microservices/book/multiplication/persistence/repository/jpa/IChallengeAttemptJpaRepository.java
@@ -1,8 +1,7 @@
 package microservices.book.multiplication.persistence.repository.jpa;
 
-import microservices.book.multiplication.persistence.repository.entity.ChallengeAttemptPersistable;
+import microservices.book.multiplication.persistence.repository.entity.ChallengeAttemptEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-public interface IChallengeAttemptJpaRepository extends JpaRepository<ChallengeAttemptPersistable, Long> {
+public interface IChallengeAttemptJpaRepository extends JpaRepository<ChallengeAttemptEntity, Long> {
 }
